f31ea9b18823e341a4fef1c5a6fc229bde316cb4
JoseRFJuniorMedibands <144800596+JoseRFJuniorMedibands@users.noreply.github.com>
2024-02-13 19:09:30 +0000

    update arquitetura de pastas

--- src/service/user_servicer.py
+++ src/service/user_servicer.py
@@ -10,14 +10,14 @@ COLLECTION_USER = "StudentCollection"
 
 
 def create_user(user_data: User):
-    return db().save_document(collection=COLLECTION_USER, document=user_data.mongo())
+    return db().save_document(collection=COLLECTION_USER, document=user_data.dict())
 
 
 def update_user(user_data: User):
     db().update_document(
         collection=COLLECTION_USER,
         key={"_id": user_data.id},
-        document=user_data.mongo(),
+        document=user_data.dict(),
     )
 
 
@@ -27,22 +27,15 @@ def delete_by_id(user_id: str):
 
 def get_list_users():
     list_users = db().get_by_filter(collection=COLLECTION_USER, key={})
-    return dict_to_user_model(list_users)
+    return [User(**user) for user in list_users]
 
 
 def get_user_by_id(user_id: str):
     result = db().get_by_key(collection=COLLECTION_USER, key={"_id": ObjectId(user_id)})
-    return User.from_mongo(result)
+    return User(**result)
 
 
 def get_user_by_name(user_name: str):
     regex = re.compile(f".*{user_name}.*", re.IGNORECASE)
     list_users = db().get_by_filter(collection=COLLECTION_USER, key={"name": regex})
-    return dict_to_user_model(list_users)
-
-
-def dict_to_user_model(list_users: List) -> List[User]:
-    users = []
-    for user in list_users:
-        users.append(User.from_mongo(user))
-    return users
+    return [User(**user) for user in list_users]
